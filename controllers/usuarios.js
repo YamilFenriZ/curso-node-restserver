f6266eef231e353941640e77eaa2ef00c8b0c0d2
Yamil Fenriz <yamilfenriz@gmail.com>
2022-04-01 23:08:32 -0400

    Primer Commit - Backend

---
+++ controllers/usuarios.js
@@ -0,0 +1,48 @@
+const { response, request } = require('express')
+
+const usuariosGet = (req = request, res = response) => {
+    const { q, nombre = "No name", apikey, page = 1, limit } = req.query;
+    res.json({
+        msg: 'get API - Controller',
+        q,
+        nombre,
+        apikey,
+        page,
+        limit
+    })
+}
+
+const usuariosPost = (req, res) => {
+    const { nombre, edad } = req.body;
+
+    res.json({
+        msg: 'post API',
+        nombre,
+        edad
+    })
+};
+const usuariosPut = (req, res) => {
+    const { id } = req.params;
+    res.json({
+        msg: 'put API',
+        id
+    })
+};
+const usuariosPatch = (req, res) => {
+    res.json({
+        msg: 'Patch API'
+    })
+};
+const usuariosDelete = (req, res) => {
+    res.json({
+        msg: 'delete API'
+    })
+};
+
+module.exports = {
+    usuariosGet,
+    usuariosPut,
+    usuariosPost,
+    usuariosPatch,
+    usuariosDelete
+}
